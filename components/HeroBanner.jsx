4df475cf4c3b79adda4e20dbe7c41c95ec5423fc
DenisFedash <0501054104d@gmail.com>
2023-04-26 21:09:58 +0300

    add code

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
